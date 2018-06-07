47985a0ba3ca8b709223821ce9db1680f992ebea
Frititati <filippo1mc@gmail.com>
2018-06-08 00:06:55 +0200

    view questions

--- application/models/question_model.php
+++ application/models/question_model.php
@@ -8,8 +8,8 @@ class question_model extends CI_Model {
 
     public function get_question($question_num = 1)
 	{
-        $query = $this->db->query("select text from question where id_question=".$question_num.";");
-        return $query->row_array(1);
+        $query = $this->db->query("select Q.text, Q.id_question, A.definition, A.count, A.value from question as Q, question_answer as A where Q.id_question=".$question_num." and A.id_question=Q.id_question order by A.count;");
+        return $query->result_array(1);
 	}
 }
 
--- application/views/question_view.php
+++ application/views/question_view.php
@@ -20,22 +20,22 @@
 			<!-- Header -->
 				<div id="header">
 						<div class="inner">
-							<form method="post" action="mailto:raizen@mail.utexas.edu?subject=Vocabulary Quiz 1" enctype="text/plain">
-
-								<P><?php echo $question_item['text']; ?><BR>
-								<input type="radio" name="1.How often do you release a software version to the costumer?" value="1-2 weeks">1-2 weeks<BR>
-								<input type="radio" name="1.How often do you release a software version to the costumer?" value="2-3 weeks">2-3 weeks<BR>
-								<input type="radio" name="1.How often do you release a software version to the costumer?" value="3-4 weeks">3-4 weeks<BR>
-								<input type="radio" name="1.How often do you release a software version to the costumer?" value="more than 4w">more than 4w<BR>
+							<!-- <form method="post" action="mailto:raizen@mail.utexas.edu?subject=Vocabulary Quiz 1" enctype="text/plain"> -->
+								<P>
+								<?php echo $question_item[0]['text']; ?><BR>
+								<?php foreach ($question_item as $answer): ?>
+									<input type="radio" name="<?php echo $answer['text'] ?>"><?php echo $answer['definition'] ?><BR>
+								<?php endforeach; ?>
 								</p>
 
 								<br>
 								<br>
 								<br>
 								<br>
-								<input type="submit" value="Send Form">
+								<!-- <input type="submit" value="Send Form">
 								<input type="reset" value="Clear Form">
-							</form>
+							</form> -->
+							<a href="<?php echo $answer['id_question']+1 ?>"><?php echo $answer['id_question']+1 ?></a>
 						</div>
 
 						<nav id="nav">
