cd47d3dc39126e1864fa56aa35f84efbab6d62b3
Alessandra3 <Ale.melchiori@alice.it>
2018-06-09 16:18:33 +0200

    db changes

--- application/views/practice_view.php
+++ application/views/practice_view.php
@@ -11,6 +11,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 <link rel = "stylesheet" type = "text/css" href = "<?php echo base_url(); ?>assets/css/main.css">
 				<link rel = "stylesheet" type = "text/css" href = "<?php echo base_url(); ?>assets/css/ie8.css">
 				<link rel = "stylesheet" type = "text/css" href = "<?php echo base_url(); ?>assets/css/font-awesome.min.css">
+				<script type='text/javascript' src="<?php echo base_url(); ?>assets/js/jquery.min.js"></script>
+				<script type='text/javascript' src="<?php echo base_url(); ?>assets/js/jquery.dropotron.js"></script>
+				<script type='text/javascript' src="<?php echo base_url(); ?>assets/js/jquery.onvisible.min.js"></script>
+				<script type='text/javascript' src="<?php echo base_url(); ?>assets/js/jquery.scrolly.min.js"></script>
+				<script type='text/javascript' src="<?php echo base_url(); ?>assets/js/main.js"></script>
+				<script type='text/javascript' src="<?php echo base_url(); ?>assets/js/skel.min.js"></script>
+				<script type='text/javascript' src="<?php echo base_url(); ?>assets/js/util.js"></script>
         </head>
       <body class="homepage">
 			<div id="page-wrapper">
@@ -23,14 +30,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 						</p>
 					</header>
 				</section>
-
 			<!-- Carousel -->
 				<section class="carousel">
-					<div class="reel">
+					<div class="reel" style="overflow: visible; transform: translate(0px,0px);">
 						<article>
 							<a href="#" class="image featured"><img src="../../images/pic07.jpg" alt="" /></a>
-								<header>
+								<header>					
 									<h3><a href="http://localhost/AgileTest/index.php/question/1">Small Releases</a></h3>
+									<h3 style="padding-top: 20px;">Points:</h3>
 								</header>
 								<p>
 									“Put a simple system into production quickly, and release new versions on a very short cycle. This practice suggests the team put a simple system into production quickly, and release new versions on a very short cycle. In XP, short iterations of 2 weeks are suggested. In Scrum, it is represented as 30-day Sprints.” 
@@ -44,6 +51,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 							<a href="#" class="image featured"><img src="../../images/pic08.jpg" alt="" /></a>
 								<header>
 									<h3><a href="#">Planning game</a></h3>
+									<h3 style="padding-top: 20px;">Points:</h3>
 								</header>
 								<p>
 									“Prioritisation of scope for next release based on a combination of business priorities and technical estimates. The ‘planning game’ is a practice of XP, while the corresponding practice in Scrum is referred to as Sprint planning. During a planning game or Sprint planning meeting, tasks for the next Sprint are prioritized based on a combination of business need and technical estimates.”
@@ -54,6 +62,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 							<a href="#" class="image featured"><img src="../../images/pic09.jpg" alt="" /></a>
 								<header>
 									<h3><a href="#">Stand-up meetings </a></h3>
+									<h3 style="padding-top: 20px;">Points:</h3>
 								</header>
 								<p>
 									“Stand-up meetings suggested by XP (or daily meetings by Scrum) are short daily status meetings where team members quickly plan the work of the day and identify any technical obstacles to fulfilling that work. Typical questions used in stand-up meetings are: “What did I work on yesterday?”, “What do I plan to work on today?” and “What is getting in my way?”.
@@ -66,6 +75,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 							<a href="#" class="image featured"><img src="../../images/pic09.jpg" alt="" /></a>
 								<header>
 									<h3><a href="#">Retrospective Meeting</a></h3>
+									<h3 style="padding-top: 20px;">Points:</h3>
 								</header>
 								<p>
 									“Reflect on method strengths and weaknesses after each cycle.”
@@ -87,6 +97,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 							<a href="#" class="image featured"><img src="../../images/pic09.jpg" alt="" /></a>
 								<header>
 									<h3><a href="#">40-Hours Week</a></h3>
+									<h3 style="padding-top: 20px;">Points:</h3>
 								</header>
 								<p>
 									“Work time is generally limited to 40 hours per week.”
@@ -148,14 +159,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 						<span class="backward" style="display: block;"></span>
 				</section>
 
-			</div>	
-			<script src="assets/js/jquery.min.js"></script>
-			<script src="assets/js/jquery.dropotron.min.js"></script>
-			<script src="assets/js/jquery.scrolly.min.js"></script>
-			<script src="assets/js/jquery.onvisible.min.js"></script>
-			<script src="assets/js/skel.min.js"></script>
-			<script src="assets/js/util.js"></script>
-			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
-			<script src="assets/js/main.js"></script>	
+			</div>		
 	</body>
 </html>
